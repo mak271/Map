05c70a43a00804ffc3b660afdaa5e8e20c0fe38e
mak271 <maks.trof.2014@mail.ru>
2021-03-06 15:27:41 +0300

    1111

--- app/src/main/java/com/example/map/MapsActivity.kt
+++ app/src/main/java/com/example/map/MapsActivity.kt
@@ -2,10 +2,8 @@ package com.example.map
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
+import android.app.PendingIntent
+import android.content.*
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
@@ -20,6 +18,7 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -27,6 +26,12 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.collect
@@ -40,30 +45,17 @@ import kotlin.math.roundToInt
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-    private lateinit var circleOptions: CircleOptions
-    private val distance = FloatArray(2)
-
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private lateinit var locationRequest: LocationRequest
-    private var currentLocation: Location? = null
-
-    val radius = 30.0
-    val ivanovo = LatLng(56.99545039, 40.96074659)
+    private lateinit var circleOptions1: CircleOptions
+    private lateinit var circleOptions2: CircleOptions
 
     private lateinit var tv_timer: TextView
-    private var myTimerTask: TimerTask? = null
-    private lateinit var timer: Timer
-    var time: Double = 0.0
 
-    var rounded: Int = 0
-    var seconds: Int = 0
-    var minutes : Int = 0
-    var hours : Int = 0
-    private var PERMISSION_ID = 1000
+    companion object {
+        val radius = 30.0
+        val ivanovo1 = LatLng(56.99545039, 40.96074659)
+        val ivanovo2 = LatLng(56.99670852, 40.96133581)
+    }
 
-
-    var service: BinderService? = null
-    var bound = false
 
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -72,46 +64,36 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_maps)
 
         tv_timer = findViewById(R.id.tv_timer)
-        timer = Timer()
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        getLastLocation()
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
-//        findViewById<Button>(R.id.btn_number).setOnClickListener {
-//            val number = service?.randomNumber
-//            Toast.makeText(this, "number: $number", Toast.LENGTH_SHORT).show()
-//        }
-
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        val intent = Intent(this, BinderService::class.java)
-        bindService(intent, connection, Context.BIND_AUTO_CREATE)
-    }
+        startService(Intent(this, MyService::class.java))
 
 
-    private val connection: ServiceConnection = object: ServiceConnection {
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            val binder = service as BinderService.LocalBinder
-            this@MapsActivity.service = binder.service
-            bound = true
-        }
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+                object : BroadcastReceiver() {
+                    override fun onReceive(context: Context?, intent: Intent?) {
+                        val result = intent?.getStringExtra(MyService.EXTRA_COUNT)
+                        tv_timer.text = result
+                    }
 
-        override fun onServiceDisconnected(name: ComponentName?) {
-            bound = false
-        }
+                }, IntentFilter(MyService.ACTION_COUNT_BROADCAST)
+        )
 
     }
+
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        circleOptions = CircleOptions()
-        circleOptions.center(ivanovo).radius(radius)
-        mMap.addCircle(circleOptions)
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ivanovo, 18F))
+        circleOptions1 = CircleOptions()
+        circleOptions2 = CircleOptions()
+        val c1 = circleOptions1.center(ivanovo1).radius(radius)
+        val c2 = circleOptions2.center(ivanovo2).radius(radius)
+        mMap.addCircle(c1)
+        mMap.addCircle(c2)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ivanovo1, 17F))
+        enableMyLocation()
 
     }
 
@@ -123,151 +105,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun resetTimer() {
-        myTimerTask?.cancel()
-        service!!.timer1 = 0.0
-        time = 0.0
-        tv_timer.text = formatTime(0,0,0)
-    }
-
-    private fun startTimer() {
-        myTimerTask = object : TimerTask() {
-            override fun run() {
-                runOnUiThread {
-                    tv_timer.text = getTimerText()
-                }
-            }
-        }
-        timer.schedule(myTimerTask, 1000, 1000)
-    }
-
-    private fun formatTime(seconds: Int, minutes: Int, hours: Int): String {
-        return String.format("%02d", hours) + " : " + String.format("%02d", minutes) + " : " + String.format("%02d", seconds)
-    }
-
-    private fun getTimerText(): String {
-
-        rounded = service!!.timer1.roundToInt()
-
-//        GlobalScope.launch(Dispatchers.Main) {
-//            service!!.doSomething().collect { value ->
-//                rounded = value.roundToInt()
-//
-//            }
-//        }
-        seconds = rounded % 86400 % 3600 % 60
-        minutes = rounded % 86400 % 3600 / 60
-        hours = rounded % 86400 / 3600
-        println(seconds)
-
-        if (rounded != 0) {
-            return formatTime(seconds, minutes, hours)
-        }
-        else
-            return formatTime(0,0,0)
-
-
-    }
 
 
 
-
-    @SuppressLint("MissingPermission")
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun getLastLocation() {
-        if(CheckPermission()) {
-            if(isLocationEnabled()) {
-                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
-                    val location = task.result
-
-                        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-                        mapFragment.getMapAsync(this)
-
-                        getNewLocation()
-
-                }
-            } else Toast.makeText(this, "Please enable your location service", Toast.LENGTH_LONG).show()
-
-        } else RequestPermission()
-    }
-
-
-    @SuppressLint("MissingPermission")
-    private fun getNewLocation() {
-        locationRequest = LocationRequest()
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationRequest.interval = 1000
-        locationRequest.fastestInterval = 1000
-        fusedLocationProviderClient.requestLocationUpdates(
-                locationRequest, locationCallback, Looper.myLooper()
-        )
-    }
-
-    private val locationCallback = object : LocationCallback() {
-        override fun onLocationResult(p0: LocationResult?) {
-            val lastLocation = p0?.lastLocation
-            currentLocation = lastLocation
-            enableMyLocation()
-            time++
-
-            Location.distanceBetween(
-                    currentLocation!!.latitude,
-                    currentLocation!!.longitude,
-                    circleOptions.center.latitude,
-                    circleOptions.center.longitude,
-                    distance
-            )
-
-            if (distance[0] <= radius) {
-                startTimer()
-            } else
-                resetTimer()
-
-
-
-        }
-    }
-
-    private fun isLocationEnabled():Boolean {
-        val locationManager : LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-                LocationManager.NETWORK_PROVIDER
-        )
-    }
-
-    private fun CheckPermission():Boolean  {
-        if(
-            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-        ) {
-            return true
-        }
-        return false
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun RequestPermission() {
-        ActivityCompat.requestPermissions(
-                this,
-                arrayOf(
-                        Manifest.permission.ACCESS_FINE_LOCATION,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                ), PERMISSION_ID
-        )
-        getLastLocation()
-    }
-
-    override fun onRequestPermissionsResult(
-            requestCode: Int,
-            permissions: Array<out String>,
-            grantResults: IntArray
-    ) {
-        if (requestCode == PERMISSION_ID) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.d("Debug:", "You have the permission")
-            }
-        }
-    }
 
 
 
